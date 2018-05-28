86a3778c742302d6f1d0156984f621850201b545
giaduc <lgduc1913@gmail.com>
2018-05-28 17:39:08 +0700

    update builder

--- src/main/java/pattern/builder/App.java
+++ src/main/java/pattern/builder/App.java
@@ -5,15 +5,20 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		MealBuilder mealBuilder = new MealBuilder();
-		
+
 		Meal vegMeal = mealBuilder.prepareVegMeal();
 		vegMeal.showItems();
 		System.out.println(vegMeal.getCost());
-		
+
 		Meal nonVegMeal = mealBuilder.prepareNonVegMeal();
 		nonVegMeal.showItems();
 		System.out.println(nonVegMeal.getCost());
-	
+
+		/* Builder */
+		BankAccount bankAccount = new BankAccount.BankAccountBuilder("giaduc", "oo24u8sf")
+				.withEmail("lgduc1913@gmail.com").withNewsLetter(true).build();
+
+		System.out.println(bankAccount.toString());
 	}
 
 }
